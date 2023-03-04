6f980c0bc903a0ac39c5161e7009955eca878152
lyh16 <41638369+lyh16@users.noreply.github.com>
2023-03-04 19:52:33 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ class AppConfig:
     # Telegram config
     TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
     TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+    TELEGRAM_TOPIC_ID = os.getenv("TELEGRAM_TOPIC_ID", "")
 
     # GitHub config
     GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET", "secret")
